fb973a9e3d6f529259cefc5b6b8409c1783ed16b
syriable <info@syriable.com>
2026-06-06 13:29:09 +0200

    add: functions

--- modules/users/src/Models/Admin.php
+++ modules/users/src/Models/Admin.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Modules\Users\Models;
 
 use App\Models\User;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasAvatar;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,7 +17,7 @@ use Modules\Users\States\AccountState;
 
 #[Fillable(['username', 'display_name', 'email', 'password', 'suspended_at', 'suspension_reason', 'last_login_at', 'last_login_ip'])]
 #[Hidden('password', 'remember_token')]
-class Admin extends User
+class Admin extends User implements FilamentUser, HasAvatar, HasName
 {
     /** @use HasFactory<AdminFactory> */
     use HasFactory;
@@ -33,5 +37,20 @@ class Admin extends User
             'suspended_at' => 'datetime',
             'last_login_at' => 'datetime',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
+    public function getFilamentName(): string
+    {
+        return $this->display_name;
+    }
+
+    public function getFilamentAvatarUrl(): ?string
+    {
+        return $this->getLastMediaUrl('avatar');
     }
 }
