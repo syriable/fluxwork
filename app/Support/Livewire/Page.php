1e182313b53ebd7260bda75b877211ef2ffb8200
syriable <info@syriable.com>
2026-06-06 13:27:39 +0200

    implement: TranslatesConventionally

--- app/Support/Livewire/Page.php
+++ app/Support/Livewire/Page.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace App\Support\Livewire;
 
 use Filament\Schemas\Schema;
+use Syriable\Filament\Plugins\Translator\Concerns\ResolvesPageLabels;
+use Syriable\Filament\Plugins\Translator\Contracts\TranslatesConventionally;
 
-abstract class Page extends Component
+abstract class Page extends Component implements TranslatesConventionally
 {
+    use ResolvesPageLabels;
+
     protected static string $layout = 'layouts.app';
 
     public function defaultForm(Schema $schema): Schema
