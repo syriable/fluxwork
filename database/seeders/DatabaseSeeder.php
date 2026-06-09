b0b587f9a72b736f1d2c87a6cbe18ab69b10d627
syriable <info@syriable.com>
2026-06-09 16:04:00 +0200

    Fix styling

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,15 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Modules\Users\Database\Seeders\AdminSeeder;
 use Modules\Users\Database\Seeders\BuyerSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
--- modules/users/src/Models/Buyer.php
+++ modules/users/src/Models/Buyer.php
@@ -7,19 +7,22 @@ namespace Modules\Users\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Carbon;
 use Modules\Users\Database\Factories\BuyerFactory;
+use Modules\Users\Observers\BuyerObserver;
 use Modules\Users\States\AccountState;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Support\LogOptions;
 
 /**
  * @property AccountState $account_state
  * @property Carbon|null $suspended_at
  * @property string|null $suspension_reason
  */
+#[ObservedBy(BuyerObserver::class)]
 #[Fillable(['username', 'display_name', 'email', 'password', 'suspended_at', 'suspension_reason', 'last_login_at', 'last_login_ip'])]
 #[Hidden('password', 'remember_token')]
 class Buyer extends User
@@ -34,7 +37,7 @@ class Buyer extends User
         return LogOptions::defaults()
             ->logAll()
             ->logOnlyDirty()
-            ->dontSubmitEmptyLogs();
+            ->dontLogEmptyChanges();
     }
 
     /**
@@ -54,6 +57,7 @@ class Buyer extends User
         ];
     }
 
+    #[\Override]
     public function name(): Attribute
     {
         return Attribute::make(
