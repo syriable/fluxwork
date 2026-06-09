69c573cd62b42f49127140e871370a392468f8eb
syriable <info@syriable.com>
2026-06-09 15:58:57 +0200

    feat: send welcone email

---
+++ modules/users/src/Mail/WelcomeBuyerMail.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Users\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Modules\Users\Models\Buyer;
+
+class WelcomeBuyerMail extends Mailable implements ShouldQueue
+{
+    use Queueable;
+    use SerializesModels;
+
+    public function __construct(public Buyer $buyer) {}
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: __('Welcome to :app', ['app' => config('app.name')]),
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'users::mail.welcome-buyer',
+            with: ['buyer' => $this->buyer],
+        );
+    }
+}
