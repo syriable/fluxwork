6899b92ba54b5a111a994630de07a61e3522fca7
syriable <info@syriable.com>
2026-06-11 18:12:31 +0200

    chore: Making it usable by different models

--- modules/users/resources/views/mail/welcome-buyer.blade.php
+++ modules/users/resources/views/mail/welcome-buyer.blade.php
@@ -1,14 +1,14 @@
 <x-mail::message>
-# Welcome, {{ $buyer->display_name }}!
+    # Welcome, {{ $model->name }}!
 
-Your account is ready. We're glad to have you on board.
+    Your account is ready. We're glad to have you on board.
 
-You can now sign in and start exploring everything we have to offer.
+    You can now sign in and start exploring everything we have to offer.
 
-<x-mail::button :url="config('app.url')">
-Get started
-</x-mail::button>
+    <x-mail::button :url="config('app.url')">
+        Get started
+    </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    {{ config('app.name') }}
 </x-mail::message>
