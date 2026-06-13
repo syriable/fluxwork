717f66edc6054596fce75f7bb0d502ce6bf512fc
syriable <info@syriable.com>
2026-06-13 17:22:30 +0200

    style: reorder AccountState interface implementations

--- modules/users/src/Enums/AccountState.php
+++ modules/users/src/Enums/AccountState.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Modules\Users\Enums;
 
-use Filament\Support\Contracts\HasLabel;
 use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
 
-enum AccountState: string implements HasLabel, HasColor, HasIcon
+enum AccountState: string implements HasColor, HasIcon, HasLabel
 {
     case Active = 'active';
     case Suspended = 'suspended';
