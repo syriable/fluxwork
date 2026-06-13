a82e41abc443d11f548b903a329b9ec34067ab74
syriable <info@syriable.com>
2026-06-13 17:14:52 +0200

    feat: add cached timezoneList helper for form selects

--- modules/users/src/Filament/Resources/Components/Form/Timezone.php
+++ modules/users/src/Filament/Resources/Components/Form/Timezone.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Users\Filament\Resources\Components\Form;
 
 use Filament\Forms\Components\Select;
+
 use function Modules\Users\timezoneList;
 
 class Timezone
